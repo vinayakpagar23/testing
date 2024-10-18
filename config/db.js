b1c41434beb0c03d53fa62250b208b90d51939ae
vinayak pagar <vinayak@vinayaks-MacBook-Pro.local>
2024-10-18 22:31:53 +0530

    testing

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(process.env.MONGO_URI, { 
+            useUnifiedTopology: true,    
+        });
         console.log('MongoDB connected');
     } catch (err) {
         console.error(err.message);
